335e9677101cb84a4d937e8340fe9505ff32030e
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-06 15:27:04 +0545

    js rating algorithm

--- photo.py
+++ photo.py
@@ -296,13 +296,13 @@ class EloTable:
 
         keys = self._photos.keys()
 
-        for i in xrange(n_iterations):
+        for i in range(n_iterations):
 
             np.random.shuffle(keys)
 
             n_matchups = n_photos / 2
 
-            for j in xrange(0, n_photos - 1, 2):
+            for j in range(0, n_photos - 1, 2):
 
                 match_up = j / 2
 
---
+++ ranking.js
@@ -0,0 +1,60 @@
+// Define a function to calculate the average rating of a photo
+function getAverageRating(photo) {
+    if (!photo.votes || photo.votes.length == 0) {
+      return 0;
+    }
+    var sum = 0;
+    for (var i = 0; i < photo.votes.length; i++) {
+      sum += photo.votes[i];
+    }
+    return sum / photo.votes.length;
+  }
+  
+  // Define a function to add a vote to a photo
+  function addVote(photo, rating) {
+    if (!photo.votes) {
+      photo.votes = [];
+    }
+    photo.votes.push(rating);
+  }
+  
+  // Define a function to sort an array of photos by average rating
+  function sortPhotosByRating(photos) {
+    photos.sort(function(a, b) {
+      return getAverageRating(b) - getAverageRating(a);
+    });
+  }
+  
+  // Example usage:
+  
+  // Define some photos
+  var photo1 = {
+    name: "Sunset at the Beach",
+    votes: [5, 4, 5, 5]
+  };
+  var photo2 = {
+    name: "Mountain Landscape",
+    votes: [3, 4, 5]
+  };
+  var photo3 = {
+    name: "Cityscape at Night",
+    votes: [5, 5]
+  };
+  var photos = [photo1, photo2, photo3];
+  
+  // Add a new vote to photo2
+  addVote(photo2, 4);
+  
+  // Sort the photos by average rating
+  sortPhotosByRating(photos);
+  
+  // Print the sorted list of photos and their ratings
+  for (var i = 0; i < photos.length; i++) {
+    console.log(photos[i].name + ": " + getAverageRating(photos[i]));
+  }
+  
+  // Output:
+  // Cityscape at Night: 5
+  // Sunset at the Beach: 4.75
+  // Mountain Landscape: 4
+  
