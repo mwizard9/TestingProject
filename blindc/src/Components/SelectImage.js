fefa20edb38eb7c28b88ee3ff97bcec168bb1296
Manish Himalaya <manishhimalaya64@gmail.com>
2023-02-07 15:22:58 +0545

    counterapp

--- blindc/src/Components/SelectImage.js
+++ blindc/src/Components/SelectImage.js
@@ -1,65 +1,77 @@
 import React, { useState } from 'react';
 
 const SelectImage = () => {
-  const imagesArray = [
-    'img-1.jpg',
-    'img-2.jpg',
-    'img-3.jpg',
-    'img-4.jpg',
-    'pack.jpg',
-    'veld.jpg',
-    'blank.jpg',
-    'daytime.jpeg',
-    'nighttime.jpeg',
-    'portait.jpeg'
-  ];
-  var usedImages = {};
-  var usedImagesCount = 0;
+    const imagesArray = [
+        { id: 1, src: 'img-1.jpg', votes: 0 },
+        { id: 2, src: 'img-2.jpg', votes: 0 },
+        { id: 3, src: 'img-3.jpg', votes: 0 },
+        { id: 4, src: 'img-4.jpg', votes: 0 },
+        { id: 5, src: 'pack.jpg', votes: 0 },
+        { id: 6, src: 'veld.jpg', votes: 0 },
+        { id: 7, src: 'blank.jpg', votes: 0 },
+        { id: 8, src: 'daytime.jpeg', votes: 0 },
+        { id: 9, src: 'nighttime.jpeg', votes: 0 },
+        { id: 10, src: 'portait.jpeg', votes: 0 },
+    ];
 
-  const [votes, setVotes] = useState({});
+    const [images, setImages] = useState(imagesArray);
 
-  const displayImage = (idx) => {
-    const num = Math.floor(Math.random() * (imagesArray.length));
-    const num2 = Math.floor(Math.random() * (imagesArray.length));
+    const updateVotes = (id) => {
+        const updatedImages = images.map((image) => {
+            if (image.id === id) {
+                image.votes += 1;
+            }
+            return image;
+        });
+        setImages(updatedImages);
+    };
 
-    if (!usedImages[num] && num !== num2) {
-      document.canvas.src = imagesArray[num];
-      document.canva.src = imagesArray[num2];
-      usedImages[num] = true;
+    const displayImage = (id) => {
+        const num = Math.floor(Math.random() * images.length);
+        const selectedImage = images[num];
+        updateVotes(selectedImage.id);
+        document.canvas.src = selectedImage.src;
+    };
 
-      usedImagesCount++;
-      if (usedImagesCount === imagesArray.length) {
-        usedImagesCount = 0;
-        usedImages = {};
-      }
-    } else {
-      displayImage();
-    }
+    const displayImage2 = (id) => {
+        const num2 = Math.floor(Math.random() * images.length);
+        const selectedImage2 = images[num2];
+        updateVotes(selectedImage2.id);
+        document.canva.src = selectedImage2.src;
+    };
 
-    setVotes({
-      ...votes,
-      [idx]: votes[idx] ? votes[idx] + 1 : 1
-    });
-  };
-
-  return (
-    <div className="container">
-      <div className="container mx-3 my-5 d-flex flex-row">
-        <div className="container">
-          <img onClick={() => displayImage(0)} src="blank.jpg" alt="canvas" name="canvas" style={{ height: '500px', width: '500px' }} />
-          <br />
-          <span className="thumb">thumb_up</span>
-          <p>Votes: {votes[0] || 0}</p>
-        </div>
-        <div className="container">
-          <img onClick={() => displayImage(1)} src="img-1.jpg" alt="canva" name="canva" style={{ height: '500px', width: '500px' }} />
-          <br />
-          <span className="thumb">thumb_up</span>
-          <p>Votes: {votes[1] || 0}</p>
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <div className="container">
+                <div className="container mx-3 my-5 d-flex flex-row">
+                    <div className="container">
+                        <img
+                            onClick={() => displayImage(1)}
+                            src="blank.jpg"
+                            alt="canvas"
+                            name="canvas"
+                            style={{ height: '500px', width: '500px' }}
+                        />
+                        <br />
+                        <span className="thumb">thumb_up</span>
+                        <span className="vote-count">{images[0].votes}</span>
+                    </div>
+                    <div className="container">
+                        <img
+                            onClick={() => displayImage2(2)}
+                            src="img-1.jpg"
+                            alt="canva"
+                            name="canva"
+                            style={{ height: '500px', width: '500px' }}
+                        />
+                        <br />
+                        <span className="thumb">thumb_up</span>
+                        <span className="vote-count">{images[1].votes}</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default SelectImage;
