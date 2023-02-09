35292e5d9d0fc3e68fd08daaa31ca930ca213a8b
Manish Himalaya <manishhimalaya64@gmail.com>
2023-02-09 10:56:42 +0545

    day 1

--- image/index.js
+++ image/index.js
@@ -1,6 +1,21 @@
 let arr = [22,33,44];
 let a = arr.map((value,index,array)=>{
+    //this can print value,index and array
 console.log(value,index,array)
 return value+index;
 })
 console.log(a)
+
+
+let b = arr.map((value)=>{
+    //it only print the value of the array
+  console.log(value)  
+  return value
+})
+console.log(b)
+
+let c= arr.map((aa,index)=>{
+    console.log(index)
+    return index
+})
+console.log(c)
