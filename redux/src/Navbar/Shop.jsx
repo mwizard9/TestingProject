c3cc72151c1bdc63fc707815a455dd425479cd24
Manish Himalaya <manishhimalaya64@gmail.com>
2023-02-23 14:36:34 +0545

    making shop item

---
+++ redux/src/Navbar/Shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h1>Diposite/Withdraw Money</h1>
+        
+      <button className="btn btn-primary mx-2">-</button>
+      Update balance
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
