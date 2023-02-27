112caa6086408c1fa8db068d97254160feac5b70
Manish Himalaya <manishhimalaya64@gmail.com>
2023-02-27 13:21:54 +0545

    complete counter using react redux toolkit

--- toolkit-counter/src/feature/counter/Counter.jsx
+++ toolkit-counter/src/feature/counter/Counter.jsx
@@ -1,29 +1,36 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment,incrementByAmount } from './counterSlice'
 
 export function Counter() {
-    const count = useSelector((state) => state.counter.value)
-    const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div>
-     
+      <div >
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
+          style={{margin:'20px'}}
         >
           Increment
         </button>
         <span>{count}</span>
         <button
+        style={{margin:'20px'}}
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           Decrement
+        </button><br/>
+        <button
+          style={{margin:'20px'}}
+          onClick={() => dispatch(incrementByAmount(100))}
+        >
+          incrementBYAmount
         </button>
       </div>
-    
+    </div>
   )
-
 }
