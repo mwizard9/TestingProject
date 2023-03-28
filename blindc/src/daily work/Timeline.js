19844b22b2c2e67517ae2ff7cf9fc0c8ff4217b3
Manish Himalaya <manishhimalaya64@gmail.com>
2023-03-28 21:00:19 +0545

    timline by abtdesign

---
+++ blindc/src/daily work/Timeline.js
@@ -0,0 +1,44 @@
+import { Radio, Timeline } from 'antd';
+import { useState } from 'react';
+const App = () => {
+  const [mode, setMode] = useState('left');
+  const onChange = (e) => {
+    setMode(e.target.value);
+  };
+  return (
+    <>
+      <Radio.Group
+        onChange={onChange}
+        value={mode}
+        style={{
+          marginBottom: 20,
+        }}
+      >
+        <Radio value="left">Left</Radio>
+        <Radio value="right">Right</Radio>
+        <Radio value="alternate">Alternate</Radio>
+      </Radio.Group>
+      <Timeline
+        mode={mode}
+        items={[
+          {
+            label: '2015-09-01',
+            children: 'Create a services',
+          },
+          {
+            label: '2015-09-01 09:12:11',
+            children: 'Solve initial network problems',
+          },
+          {
+            children: 'Technical testing',
+          },
+          {
+            label: '2015-09-01 09:12:11',
+            children: 'Network problems being solved',
+          },
+        ]}
+      />
+    </>
+  );
+};
+export default App;
